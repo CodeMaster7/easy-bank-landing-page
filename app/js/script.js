16f62489554b84f64a5ddbb37d885cdae53f446e
sam <sammybonfanti@gmail.com>
2021-05-29 02:38:18 -0700

    fixes on the header

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,17 @@
 "use strict"
 const _btnHamburger = document.querySelector('#_btnHamburger')
 const header = document.querySelector('.header')
+const overlay = document.querySelector('.overlay')
 
 _btnHamburger.addEventListener('click', () => {
     // header.classList.toggle('open')
-    if (header.classList.contains('open')) {
+    if (header.classList.contains('open')) { // close Hamburger Menu
         header.classList.remove('open')
-    } else {
+        overlay.classList.toggle('fade-in')
+		overlay.classList.toggle('fade-out', !overlay.classList.contains('fade-in'))
+    } else { // Open Hamburger Menu
         header.classList.add('open')
+        overlay.classList.toggle('fade-in')
+		overlay.classList.toggle('fade-out', !overlay.classList.contains('fade-in'))
     }
 })
