6aa1d2b20ad309078fda6853f808f5cb39d343b1
sam <sammybonfanti@gmail.com>
2021-05-29 00:17:32 -0700

    created overlay

--- app/js/script.js
+++ app/js/script.js
@@ -2,6 +2,7 @@
 const _btnHamburger = document.querySelector('#_btnHamburger')
 
 _btnHamburger.addEventListener('click', () => {
+    // _btnHamburger.classList.toggle('open')
     if (_btnHamburger.classList.contains('open')) {
         _btnHamburger.classList.remove('open')
     } else {
