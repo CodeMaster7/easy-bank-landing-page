fb403ad6465d039838c2a0a2d36340e5e8058250
sam <sammybonfanti@gmail.com>
2021-05-29 03:09:38 -0700

    header and nav are done

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,24 @@
 "use strict"
 const _btnHamburger = document.querySelector('#_btnHamburger')
+const body = document.querySelector('body')
 const header = document.querySelector('.header')
 const overlay = document.querySelector('.overlay')
+const fadeElems = document.querySelectorAll('.has-fade')
 
 _btnHamburger.addEventListener('click', () => {
-    // header.classList.toggle('open')
     if (header.classList.contains('open')) { // close Hamburger Menu
+        body.classList.remove('noscroll')
         header.classList.remove('open')
-        overlay.classList.toggle('fade-in')
-		overlay.classList.toggle('fade-out', !overlay.classList.contains('fade-in'))
+        fadeElems.forEach( element => {
+			element.classList.remove('fade-in')
+			element.classList.add('fade-out')
+		})
     } else { // Open Hamburger Menu
+        body.classList.add('noscroll')
         header.classList.add('open')
-        overlay.classList.toggle('fade-in')
-		overlay.classList.toggle('fade-out', !overlay.classList.contains('fade-in'))
+        fadeElems.forEach( element => {
+            element.classList.remove('fade-out')
+            element.classList.add('fade-in')
+        })
     }
 })
