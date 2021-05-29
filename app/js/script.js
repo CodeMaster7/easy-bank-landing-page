0fa8e0ec89cbea67097cea1f87bdb35641d45a89
sam <sammybonfanti@gmail.com>
2021-05-29 00:54:35 -0700

    overlay fade in animation working

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,12 @@
 "use strict"
 const _btnHamburger = document.querySelector('#_btnHamburger')
+const header = document.querySelector('.header')
 
 _btnHamburger.addEventListener('click', () => {
-    // _btnHamburger.classList.toggle('open')
-    if (_btnHamburger.classList.contains('open')) {
-        _btnHamburger.classList.remove('open')
+    // header.classList.toggle('open')
+    if (header.classList.contains('open')) {
+        header.classList.remove('open')
     } else {
-        _btnHamburger.classList.add('open')
+        header.classList.add('open')
     }
 })
